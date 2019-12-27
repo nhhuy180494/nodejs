e7ba3f19f38a01c2eb4f01dec4df475b308cbf0f
nhhuy1804 <anhhuyno8@gmail.com>
2019-12-27 16:52:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+var express = require('express');
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+// view engine setup
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
+
+// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+io.on('connection', function(socket) {
+
+    socket.on('room-joined', (room) => {
+        socket.join(room);
+        io.to(room).emit("user-joined", socket.id, io.sockets.adapter.rooms[room].length, Object.keys(io.sockets.adapter.rooms[room].sockets));
+    });
+
+    socket.on('signal', (toId, message) => {
+        io.to(toId).emit('signal', socket.id, message);
+    });
+
+    socket.on("message", function(data) {
+        io.sockets.emit("broadcast-message", socket.id, data);
+    })
+
+    socket.on('disconnect', function() {
+        io.sockets.emit("user-left", socket.id);
+    })
+});
+
+server.listen(3000, function() {
+    console.log("Express server listening on port %d in %s mode", server.address().port, server.settings);
+});
---
+++ public/javascripts/index.js
@@ -0,0 +1,129 @@
+var localVideo;
+var firstPerson = false;
+var socketCount = 0;
+var socketId;
+var localStream;
+var connections = [];
+
+var peerConnectionConfig = {
+    'iceServers': [
+        { 'urls': 'stun:stun.services.mozilla.com' },
+        { 'urls': 'stun:stun.l.google.com:19302' },
+    ]
+};
+
+function pageReady() {
+    localVideo = document.getElementById('localVideo');
+
+    var constraints = {
+        video: true
+    };
+
+    if (navigator.mediaDevices.getDisplayMedia) {
+        navigator.mediaDevices.getDisplayMedia(constraints)
+            .then(getUserMediaSuccess)
+            .then(function() {
+
+                socket = io.connect("http://localhost:3000");
+                socket.on('signal', gotMessageFromServer);
+
+                socket.on('connect', function() {
+
+                    socketId = socket.id;
+
+                    socket.emit("room-joined", $('#room').val());
+
+                    socket.on('user-left', function(id) {
+                        var video = document.querySelector('[data-socket="' + id + '"]');
+                        var parentDiv = video.parentElement;
+                        video.parentElement.parentElement.removeChild(parentDiv);
+                    });
+
+
+                    socket.on('user-joined', function(id, count, clients) {
+                        clients.forEach(function(socketListId) {
+                            if (!connections[socketListId]) {
+                                connections[socketListId] = new RTCPeerConnection(peerConnectionConfig);
+                                //Wait for their ice candidate       
+                                connections[socketListId].onicecandidate = function() {
+                                    if (event.candidate != null) {
+                                        console.log('SENDING ICE');
+                                        socket.emit('signal', socketListId, JSON.stringify({ 'ice': event.candidate }));
+                                    }
+                                }
+
+                                //Wait for their video stream
+                                connections[socketListId].onaddstream = function() {
+                                    gotRemoteStream(event, socketListId)
+                                }
+
+                                //Add the local video stream
+                                connections[socketListId].addStream(localStream);
+                            }
+                        });
+
+                        //Create an offer to connect with your local description
+
+                        if (count >= 2) {
+                            connections[id].createOffer().then(function(description) {
+                                connections[id].setLocalDescription(description).then(function() {
+                                    // console.log(connections);
+                                    socket.emit('signal', id, JSON.stringify({ 'sdp': connections[id].localDescription }));
+                                }).catch(e => console.log(e));
+                            });
+                        }
+                    });
+                })
+
+            });
+    } else {
+        alert('Your browser does not support getUserMedia API');
+    }
+}
+
+function getUserMediaSuccess(stream) {
+    localStream = stream;
+    localVideo.srcObject = stream;
+}
+
+function gotRemoteStream(event, id) {
+
+    var videos = document.querySelectorAll('video'),
+        video = document.createElement('video'),
+        div = document.createElement('div')
+
+    video.setAttribute('data-socket', id);
+    video.srcObject = event.stream;
+    video.autoplay = true;
+    video.muted = true;
+    video.playsinline = true;
+
+    div.appendChild(video);
+    document.querySelector('.videos').appendChild(div);
+}
+
+function gotMessageFromServer(fromId, message) {
+
+    //Parse the incoming signal
+    var signal = JSON.parse(message)
+
+    //Make sure it's not coming from yourself
+    if (fromId != socketId) {
+
+        if (signal.sdp) {
+            connections[fromId].setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
+                if (signal.sdp.type == 'offer') {
+                    connections[fromId].createAnswer().then(function(description) {
+                        connections[fromId].setLocalDescription(description).then(function() {
+                            socket.emit('signal', fromId, JSON.stringify({ 'sdp': connections[fromId].localDescription }));
+                        }).catch(e => console.log(e));
+                    }).catch(e => console.log(e));
+                }
+            }).catch(e => console.log(e));
+        }
+
+        if (signal.ice) {
+            connections[fromId].addIceCandidate(new RTCIceCandidate(signal.ice)).catch(e => console.log(e));
+        }
+    }
+}
